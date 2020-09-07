7ac79c884a7ac5d940d25a9366d703d8bb021483
user <user@localhost>
2020-09-07 21:04:04 +0200

    initial commit

---
+++ Scheduler.API/Startup.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Scheduler.API.Core;
+using Scheduler.API.ViewModels.Mappings;
+using Scheduler.Data;
+using Scheduler.Data.Abstract;
+using Scheduler.Data.Repositories;
+
+namespace Scheduler.API
+{
+    public class Startup
+    {
+        private static string _applicationPath = string.Empty;
+        string sqlConnectionString = string.Empty;
+        bool useInMemoryProvider = false;
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            _applicationPath = env.WebRootPath;
+            // Setup configuration sources.
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                // This reads the configuration keys from the secret store.
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string sqlConnectionString = Configuration.GetConnectionString("DefaultConnection");
+            try
+            {
+                useInMemoryProvider = bool.Parse(Configuration["AppSettings:InMemoryProvider"]);
+            }
+            catch { }
+
+            services.AddDbContext<SchedulerContext>(options => {
+                switch (useInMemoryProvider)
+                {
+                    case true:
+                        options.UseInMemoryDatabase();
+                        break;
+                    default:
+                        options.UseSqlServer(sqlConnectionString,
+                            b => b.MigrationsAssembly("Scheduler.API"));
+                        break;
+                }
+            });
+
+
+            // Repositories
+            services.AddScoped<IScheduleRepository, ScheduleRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IAttendeeRepository, AttendeeRepository>();
+
+            // Automapper Configuration
+            AutoMapperConfiguration.Configure();
+
+            // Enable Cors
+            services.AddCors();
+
+            // Add MVC services to the services container.
+            services.AddMvc()
+                .AddJsonOptions(opts =>
+                {
+                    // Force Camel Case to JSON
+                    opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            // Add MVC to the request pipeline.
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
+
+            app.UseExceptionHandler(
+                builder =>
+                {
+                    builder.Run(
+                        async context =>
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+                            var error = context.Features.Get<IExceptionHandlerFeature>();
+                            if (error != null)
+                            {
+                                context.Response.AddApplicationError(error.Error.Message);
+                                await context.Response.WriteAsync(error.Error.Message).ConfigureAwait(false);
+                            }
+                        });
+                });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                //routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+
+            SchedulerDbInitializer.Initialize(app.ApplicationServices);
+        }
+    }
+}
---
+++ Scheduler.API/ViewModels/Validations/ScheduleViewModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+
+namespace Scheduler.API.ViewModels.Validations
+{
+    public class ScheduleViewModelValidator : AbstractValidator<ScheduleViewModel>
+    {
+        public ScheduleViewModelValidator()
+        {
+            RuleFor(s => s.TimeEnd).Must((start, end) =>
+            {
+                return DateTimeIsGreater(start.TimeStart, end);
+            }).WithMessage("Schedule's End time must be greater than Start time");
+        }
+
+        private bool DateTimeIsGreater(DateTime start, DateTime end)
+        {
+            return end > start;
+        }
+    }
+}
